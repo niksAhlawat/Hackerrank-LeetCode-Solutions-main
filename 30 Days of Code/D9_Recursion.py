822f986f4dd70f43cd1f213fa2879d98743742d3
niksAhlawat <nikhilahlawat.nikk@outlook.com>
2021-12-18 13:45:49 +0530

    Update 18/12/2021

---
+++ 30 Days of Code/D9_Recursion.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'factorial' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def factorial(n):
+    # Write your code here
+    return 1 if (n==1 or n==0) else n * factorial(n - 1);
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Hackerrank-LeetCode-Solutions
+
+#This repo contains some of Hackerrank and Leetcode Programming Solutions.
+#This repo will be continously updated time to time as I go through the exercises.
+
+#If someone want to commit or add to this repo, they are most welcome.
+
